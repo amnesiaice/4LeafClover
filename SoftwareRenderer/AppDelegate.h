b95f40bedfe42bbfe0cb3f1943f25ee305ca45a6
amnesiaice <jxw9391@qq.com>
2019-05-07 22:06:51 +0800

    Initial Commit

---
+++ SoftwareRenderer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoftwareRenderer
+//
+//  Created by Amn Ice on 2019/5/7.
+//  Copyright © 2019 Amn Ice. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
