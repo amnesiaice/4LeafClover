b40f80385ec98e9849a3118f3b632bad523ec147
amnesiaice <jxw9391@qq.com>
2019-05-14 21:50:02 +0800

    re-initial project to use the swift project and use story board for mac version

---
+++ 4LeafClover/4LeafClover/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  4LeafClover
+//
+//  Created by Amn Ice on 2019/5/14.
+//  Copyright © 2019 Amn Ice. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
